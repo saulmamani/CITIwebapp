0a2f61e09212d4d95ca509b67926f8aec357edd1
saul mamani <luas0_1@yahoo.es>
2023-11-11 11:25:33 -0400

    Se agrego Id , Key y Required a la clase Usuario

--- CITIwebApp/Models/Usuario.cs
+++ CITIwebApp/Models/Usuario.cs
@@ -1,9 +1,13 @@
 ﻿using CITIwebApp.Dtos;
+using System.ComponentModel.DataAnnotations;
 
 namespace CITIwebApp.Models
 {
     public class Usuario
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? NombreCompleto { get; set; }

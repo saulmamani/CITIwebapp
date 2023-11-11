ea3851f8a26ff5d4537a3e7ce6fa4e441793af2e
saul mamani <luas0_1@yahoo.es>
2023-11-11 11:21:36 -0400

    Agregado Key y Required a Ingeniero

--- CITIwebApp/Models/Ingeniero.cs
+++ CITIwebApp/Models/Ingeniero.cs
@@ -1,10 +1,13 @@
 ﻿using CITIwebApp.Dtos;
+using System.ComponentModel.DataAnnotations;
 
 namespace CITIwebApp.Models
 {
     public class Ingeniero
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public int Rni { get; set; }
         public string? Ci { get; set; }
         public string? NombreCompleto { get; set; }

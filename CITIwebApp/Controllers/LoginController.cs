0a37692c93d0401071b6cf8cea3f8a074b9fa2e3
saul mamani <luas0_1@yahoo.es>
2023-12-07 18:36:37 -0400

    solo compilados

---
+++ CITIwebApp/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using CITIwebApp.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CITIwebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        MiContext _miContext;
+        public LoginController(MiContext miContext)
+        {
+            _miContext = miContext;
+        }
+
+        //GET
+        public IActionResult Index()
+        {
+            //para que muestre la pantalla del login
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string correo, string contrasena)
+        {
+            var usuario = await _miContext.Usuarios
+                            .Where(x => x.Email == correo && x.Password == contrasena)
+                            .FirstOrDefaultAsync();
+
+            if (usuario != null) //se ha encontrado al usuario
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else 
+            {
+                TempData["LoginError"] = "Cuenta o Password incorrectos";
+                return Redirect("Index");
+            }
+        }
+    }
+}
